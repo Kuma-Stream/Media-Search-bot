3ebb7fbbe46841d9131f4078b91072debe6983b8
Kuma-Stream <121804937+Kuma-Stream@users.noreply.github.com>
2023-09-22 16:25:53 +0700

    a

--- plugins/inline.py
+++ plugins/inline.py
@@ -41,7 +41,7 @@ async def answer(bot, query):
     for file in files:
         results.append(
             InlineQueryResultCachedDocument(
-                title=file.file_name,
+                title=file.caption,
                 document_file_id=file.file_id,
                 caption=file.caption or "",
                 description=f'Size: {size_formatter(file.file_size)}\nType: {file.file_type}',
